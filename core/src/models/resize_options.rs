aee5d83512a6bdbdcf6d3d271aed5356076e089b
scombat <6457707+scombat@users.noreply.github.com>
2025-07-06 18:18:24 +0200

    fix(core/resize): use nearest as default filter strategy instead of triangle

--- core/src/models/resize_options.rs
+++ core/src/models/resize_options.rs
@@ -26,7 +26,7 @@ impl Default for ResizeOptions {
             height: None,
             dpr: 1.0,
             strategy: AspectRatioStrategy::Fit,
-            filter: FilterType::Triangle,
+            filter: FilterType::Nearest,
         }
     }
 }
